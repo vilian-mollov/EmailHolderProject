a67519b25fa5f60d4339b13197ef201d142cd606
Avalon <vilian3737@gmail.com>
2022-09-09 19:01:55 +0300

    Added HomeController GET and POST methods and also made some calibrations in index.html so can be visualized properly

--- src/main/java/EmailHolder/Controller/HomeController.java
+++ src/main/java/EmailHolder/Controller/HomeController.java
@@ -1,20 +1,39 @@
 package EmailHolder.Controller;
 
 
+import EmailHolder.entity.Email;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
     // GET, POST, PUT, DELETE
 
+    List<Email> emailsList;
+
+    public HomeController() {
+        this.emailsList = new ArrayList<>();
+    }
+
 
     //localhost:8080
-    @ResponseBody
-    @GetMapping(value = "/home")
-    public String home(){
-        return  "<h1>Hello JAVA - Jedi</h1>";
+    @GetMapping("/")
+    public ModelAndView home(ModelAndView modelAndView) {
+        modelAndView.setViewName("index");
+        modelAndView.addObject("emailsList", this.emailsList);
+        return modelAndView;
     }
+
+    @PostMapping
+    public String add(Email email){
+        this.emailsList.add(email);
+        return "redirect:/";
+    }
+
 
 }
